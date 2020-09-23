40aa4d2eb5d67e67225f8998740669f5e37fe5d4
brainz <piusunimke@gmail.com>
2020-09-23 22:35:37 +0100

    completed the project

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -3,7 +3,12 @@ import PropTypes from 'prop-types';
 const UserItem = ({ user: { avatar_url, html_url, login } }) => {
   return (
     <div className='card text-center'>
-      <img src={avatar_url} className='round-img' style={{ width: '60px' }} />
+      <img
+        alt='logo'
+        src={avatar_url}
+        className='round-img'
+        style={{ width: '60px' }}
+      />
       <h3>{login}</h3>
       <div>
         <a href={html_url} className='btn btn-dark btn-small my-1'>
